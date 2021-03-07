828787aac07875c41c6e7aed6d77b28e820ace54
killerbossoriginal <73131550+killerbossoriginal@users.noreply.github.com>
2021-03-07 19:18:54 +0100

    calcolatore di età

---
+++ index.js
@@ -0,0 +1,6 @@
+var nome = imput("inserisci nome")
+var cognome = imput("inserisci cognome")
+var anno = imput("inserisci l'anno di nascita")
+var eta = 2020 - anno
+console.log("ti chiami " + nome + cognome)
+console.log("hai " + eta)
